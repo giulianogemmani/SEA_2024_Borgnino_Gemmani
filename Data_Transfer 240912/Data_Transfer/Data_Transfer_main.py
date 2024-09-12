c0a88fffdf7de110d95b472f5ec255708ab05f98
MicheleBorgnino <104070372+MicheleBorgnino@users.noreply.github.com>
2024-09-12 16:20:37 +0200

    new data transfer project with new features

---
+++ Data_Transfer 240912/Data_Transfer/Data_Transfer_main.py
@@ -0,0 +1,11 @@
+'''
+Created on July 15, 2024
+
+@author: MB
+'''
+
+import picture_transfer.Creator_PowerFactory as mainapp  # @UnresolvedImport
+
+
+if __name__ == '__main__':  
+    mainapp.run()
