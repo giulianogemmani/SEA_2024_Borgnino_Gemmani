dfc40c7a3ecd5b084f6b4123e58dec312c47ba09
MicheleBorgnino <104070372+MicheleBorgnino@users.noreply.github.com>
2024-09-17 19:51:34 +0200

    changes to matlab and simulink

--- Data_Transfer/matlab_runner.py
+++ Data_Transfer/matlab_runner.py
@@ -7,4 +7,4 @@ print("Current Working Directory:", os.getcwd())
 # Start MATLAB engine
 eng = matlab.engine.start_matlab()
 eng.addpath("C:\\Users\\borgn\\OneDrive\\Desktop\\progetto_software\\SEA_2024_Borgnino_Gemmani")
-eng.matlab_main('PPC_scheme_04072024_a')
+eng.matlab_main('example_simulink_scheme2023b')
